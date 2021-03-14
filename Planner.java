4b103253c010349ca5cde274b5193c9c7219748a
Alberto Pluecker <albertopluecker@gmail.com>
2021-03-14 22:25:15 +0100

    First commit

---
+++ Planner.java
@@ -0,0 +1,38 @@
+    /*
+    You need to code a planner for meetings.
+The idea is that you input the different meetings + how long they take, and your code will give you the best planned schedule for this
+- A day starts at 8am and finishes at 12pm 
+- 12pm is 1h lunch break
+- the feedback round can start not earlier than 4pm and not later than 5pm, everyday ends with a feedback round
+- Input is supposed to look like "Meeting 30min" or "Meeting Blitz" (Blitz = 10min)
+- no need to leave gaps inbetween meetings
+- If one day is full, the next day will be used
+- Output is supposed to look like
+Day 1
+08:00AM Meeting 30min
+    
+
+    1. Input: Add new meeting (Time, duration)
+    2. 5 days, 8:00-12:00, 13:00-00:00
+    3. Feedback round: start between 16:00 and 17:00, once every day
+    4. If day full -> add to next day
+    5. Show output table
+    */
+
+public static void main(String[] args) {
+
+    
+}
+
+
+
+
+public static void Weekday(){
+    //Class should have a Day value (1-5) and a Duration value (10 mins to 60 mins)
+    
+}
+
+public static void AddMeeting(String meetingDescription , int meetingDuration ){
+        
+    }
+    
